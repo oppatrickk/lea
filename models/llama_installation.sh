113af18073fcfb90f2fc39124341b9208b4343b7
John Patrick Prieto <70645552+oppatrickk@users.noreply.github.com>
2024-03-06 04:03:58 +0800

    feat(core): added llama 2 chatbot

---
+++ models/llama_installation.sh
@@ -0,0 +1,5 @@
+# Main LLM Page
+https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF
+
+# What I installed
+https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF/blob/main/llama-2-7b-chat.Q4_K_M.gguf
