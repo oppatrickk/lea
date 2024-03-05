536ed128945039727f505426f6181a12066d1116
John Patrick Prieto <70645552+oppatrickk@users.noreply.github.com>
2024-03-06 04:17:19 +0800

    docs(install): removed api docs

--- core/intall.sh
+++ core/intall.sh
@@ -11,10 +11,3 @@ pip install tiktoken
 pip install chromadb 
 pip install unstructured
 pip install llama-cpp-python
-
-# * API Key
-Go to https://platform.openai.com/ and copy your API Key
-
-Create a file called .env in the root of your project and add the following line to it:
-
-OPENAI_API_KEY="YOUR_API_KEY"
