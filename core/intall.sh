15de1e4a7592adf9c771b6209025dc81d617cd63
John Patrick Prieto <70645552+oppatrickk@users.noreply.github.com>
2024-03-06 04:14:16 +0800

    docs(install): added installation docs

--- core/intall.sh
+++ core/intall.sh
@@ -4,8 +4,17 @@
 
 pip install langchain 
 pip install langchain-openai
+pip install langchain_community
 pip install openai 
 pip install datasets 
 pip install tiktoken
 pip install chromadb 
 pip install unstructured
+pip install llama-cpp-python
+
+# * API Key
+Go to https://platform.openai.com/ and copy your API Key
+
+Create a file called .env in the root of your project and add the following line to it:
+
+OPENAI_API_KEY="YOUR_API_KEY"
